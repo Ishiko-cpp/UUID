c9a4efc91582ccf5b2c9bd71ab8073af1ef60901
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-02 12:23:06 +0100

    Add files

---
+++ Include/Ishiko/UUIDs/UUIDVersion4Generator.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/UUIDs/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_UUIDS_UUIDVERSION4GENERATOR_H_
+#define _ISHIKO_UUIDS_UUIDVERSION4GENERATOR_H_
+
+#endif
