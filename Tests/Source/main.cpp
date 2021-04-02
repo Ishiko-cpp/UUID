7257f8b93c976c031f0d837302b0de22aaa03b99
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-02 01:17:50 +0100

    Update tests

---
+++ Tests/Source/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/UUIDs/blob/master/LICENSE.txt
+*/
+
+#include "Ishiko/TestFramework/TestFrameworkCore.h"
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoUUIDs");
+
+    return theTestHarness.run();
+}
