6f88d27cfb746b669daa84b673fbe72a7a34affd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-03 14:11:09 +0100

    Implement ErrorCategory

--- Include/Ishiko/UUIDs/ErrorCategory.h
+++ Include/Ishiko/UUIDs/ErrorCategory.h
@@ -7,4 +7,33 @@
 #ifndef _ISHIKO_UUIDS_ERRORCATEGORY_H_
 #define _ISHIKO_UUIDS_ERRORCATEGORY_H_
 
+#include <Ishiko/Errors/ErrorCategory.h>
+#include <Ishiko/Errors/Error.h>
+
+namespace Ishiko
+{
+namespace UUIDs
+{
+
+class ErrorCategory : public Ishiko::ErrorCategory
+{
+public:
+    enum EErrorValues
+    {
+        eGeneric = -1
+    };
+
+    static const ErrorCategory& Get() noexcept;
+
+    const char* name() const noexcept override;
+
+private:
+    ErrorCategory() noexcept = default;
+};
+
+void Fail(Error& error, ErrorCategory::EErrorValues value) noexcept;
+
+}
+}
+
 #endif
--- Source/ErrorCategory.cpp
+++ Source/ErrorCategory.cpp
@@ -5,3 +5,27 @@
 */
 
 #include "ErrorCategory.h"
+
+namespace Ishiko
+{
+namespace UUIDs
+{
+
+const ErrorCategory& ErrorCategory::Get() noexcept
+{
+    static ErrorCategory theCategory;
+    return theCategory;
+}
+
+const char* ErrorCategory::name() const noexcept
+{
+    return "Ishiko::UUIDs::ErrorCategory";
+}
+
+void Fail(Error& error, ErrorCategory::EErrorValues value) noexcept
+{
+    error.fail(value, ErrorCategory::Get());
+}
+
+}
+}
