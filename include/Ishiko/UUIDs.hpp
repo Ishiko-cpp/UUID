129debdbac36271e037fc64105b1672fddd6b62f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-11-08 22:53:49 +0000

    Updates

--- include/Ishiko/UUIDs.hpp
+++ include/Ishiko/UUIDs.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2021-2022 Xavier Leclercq
+    Copyright (c) 2021-2023 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/uuids/blob/main/LICENSE.txt
 */
@@ -7,7 +7,7 @@
 #ifndef GUARD_ISHIKO_CPP_UUIDS_HPP
 #define GUARD_ISHIKO_CPP_UUIDS_HPP
 
-#include "linkoptions.h"
+#include "UUIDs/linkoptions.h"
 #include "UUIDs/PrecomputedUUIDGenerator.h"
 #include "UUIDs/UUID.h"
 #include "UUIDs/UUIDsErrorCategory.hpp"
